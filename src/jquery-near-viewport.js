a3f3bc5f73f5f4ba244be58f058298d6c57d767a
cobbdb <cobbdb@gmail.com>
2014-04-22 21:26:29 -0700

    Down to 300 bytes and -much- faster.. all native js

--- src/jquery-near-viewport.js
+++ src/jquery-near-viewport.js
@@ -1,31 +1,31 @@
-(function ($) {
-    $.extend($.expr[':'], {
-        /**
-         *  @see https://developer.mozilla.org/en-US/docs/Web/API/Window.scrollY
-         */
-        'near-viewport': function (el, i, meta) {
-            var margin = parseInt(meta[3]) || 0;
+/**
+ * :near-viewport
+ * the jQuery selector to find things near the viewport.
+ * @author Dan Cobb
+ * @license MIT
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/Window.scrollY
+ */
+jQuery.expr[':']['near-viewport'] = function (el, i, meta) {
+    var margin = parseInt(meta[3]) || 0;
 
-            var winTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-            var winHeight = window.innerHeight || $(window).height();
-            var winBottom = winTop + winHeight;
+    var winTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+    var winHeight = document.documentElement.clientHeight;
+    var winBottom = winTop + winHeight;
 
-            var rect = el.getBoundingClientRect();
-            var elTop = rect.top + winTop - margin;
-            var elBottom = rect.bottom + winTop + margin;
+    var rect = el.getBoundingClientRect();
+    var elTop = rect.top + winTop - margin;
+    var elBottom = rect.bottom + winTop + margin;
 
-            // Debug panel (removed by minification)
-            /*console.log('~~ Var dump:');
-            console.log('meta: ' + meta);
-            console.log('margin: ' + margin);
-            console.log('top: ' + elTop);
-            console.log('True top: ' + (elTop + margin));
-            console.log('bottom: ' + elBottom);
-            console.log('type of bottom: ' + typeof elBottom);
-            console.log('windowTop: ' + winTop);
-            console.log('windowHeight: ' + winHeight);
-            console.log('windowBottom: ' + winBottom);*/
-            return elBottom > winTop && elTop < winBottom;
-        }
-    });
-}(jQuery));
+    // Debug panel (removed during minification)
+    /*console.log('~~ Var dump:');
+    console.log('meta: ' + meta);
+    console.log('margin: ' + margin);
+    console.log('top: ' + elTop);
+    console.log('True top: ' + (elTop + margin));
+    console.log('bottom: ' + elBottom);
+    console.log('type of bottom: ' + typeof elBottom);
+    console.log('windowTop: ' + winTop);
+    console.log('windowHeight: ' + winHeight);
+    console.log('windowBottom: ' + winBottom);*/
+    return elBottom > winTop && elTop < winBottom;
+};
