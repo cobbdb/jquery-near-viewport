1bb1f51fcfe3e5bccdaa8ea715243d78064d5ac5
cobbdb <cobbdb@gmail.com>
2014-12-03 00:35:47 -0800

    Update SL conf

--- tasks/saucelabs-jasmine.js
+++ tasks/saucelabs-jasmine.js
@@ -13,7 +13,9 @@ module.exports = function (grunt) {
                 concurrency: 3,
                 testname: ':near-viewport',
                 browsers: browsers,
-                throttled: 6
+                throttled: 6,
+                maxRetries: 1,
+                pollInterval: 4000
             }
         }
     });
@@ -21,12 +23,20 @@ module.exports = function (grunt) {
 
 var browsers = [{
     browserName: 'firefox',
+    platform: 'Linux',
+    version: '28'
+}, {
+    browserName: 'firefox',
     platform: 'Windows 8',
     version: '29'
 }, {
     browserName: 'chrome',
     platform: 'Windows 7',
     version: '34'
+}, {
+    browserName: 'chrome',
+    platform: 'Windows XP',
+    version: '33'
 }, {
     browserName: 'internet explorer',
     platform: 'Windows XP',
@@ -41,6 +51,10 @@ var browsers = [{
     version: '10'
 }, {
     browserName: 'internet explorer',
+    platform: 'Windows 8',
+    version: '10'
+}, {
+    browserName: 'internet explorer',
     platform: 'Windows 8.1',
     version: '11'
 }, {
@@ -48,14 +62,32 @@ var browsers = [{
     platform: 'OS X 10.6',
     version: '5'
 }, {
+    browserName: 'safari',
+    platform: 'OS X 10.8',
+    version: '6'
+}, {
+    browserName: 'safari',
+    platform: 'OS X 10.9',
+    version: '7'
+}, {
     browserName: 'opera',
     platform: 'Linux',
+    version: '12'
+}, {
+    browserName: 'opera',
+    platform: 'Windows 7',
     version: '12'
 }, {
     browserName: 'iphone',
     platform: 'OS X 10.9',
     version: '7.1',
     deviceName: 'iPhone',
+    'device-orientation': 'portrait'
+}, {
+    browserName: 'ipad',
+    platform: 'OS X 10.9',
+    version: '7.0',
+    deviceName: 'iPad',
     'device-orientation': 'portrait'
 }, {
     browserName: 'ipad',
