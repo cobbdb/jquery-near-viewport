86ba3effd3cd1f209ef13dcb1e4b51a46a16aa5c
cobbdb <cobbdb@gmail.com>
2014-12-03 00:24:02 -0800

    Reduce number of SL combos

--- tasks/saucelabs-jasmine.js
+++ tasks/saucelabs-jasmine.js
@@ -21,20 +21,12 @@ module.exports = function (grunt) {
 
 var browsers = [{
     browserName: 'firefox',
-    platform: 'Linux',
-    version: '28'
-}, {
-    browserName: 'firefox',
     platform: 'Windows 8',
     version: '29'
 }, {
     browserName: 'chrome',
     platform: 'Windows 7',
     version: '34'
-}, {
-    browserName: 'chrome',
-    platform: 'Windows XP',
-    version: '33'
 }, {
     browserName: 'internet explorer',
     platform: 'Windows XP',
@@ -49,10 +41,6 @@ var browsers = [{
     version: '10'
 }, {
     browserName: 'internet explorer',
-    platform: 'Windows 8',
-    version: '10'
-}, {
-    browserName: 'internet explorer',
     platform: 'Windows 8.1',
     version: '11'
 }, {
@@ -60,32 +48,14 @@ var browsers = [{
     platform: 'OS X 10.6',
     version: '5'
 }, {
-    browserName: 'safari',
-    platform: 'OS X 10.8',
-    version: '6'
-}, {
-    browserName: 'safari',
-    platform: 'OS X 10.9',
-    version: '7'
-}, {
     browserName: 'opera',
     platform: 'Linux',
-    version: '12'
-}, {
-    browserName: 'opera',
-    platform: 'Windows 7',
     version: '12'
 }, {
     browserName: 'iphone',
     platform: 'OS X 10.9',
     version: '7.1',
     deviceName: 'iPhone',
-    'device-orientation': 'portrait'
-}, {
-    browserName: 'ipad',
-    platform: 'OS X 10.9',
-    version: '7.0',
-    deviceName: 'iPad',
     'device-orientation': 'portrait'
 }, {
     browserName: 'ipad',
